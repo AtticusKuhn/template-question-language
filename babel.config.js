85878eaa7fbe5ca9d7bdb143f24d16d4470c321c
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-07-29 18:01:32 -0700

    moved stuff

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js
-module.exports = {
-    "plugins": [],
-    presets: [
-        ["next/babel"],
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-    ],
-};
