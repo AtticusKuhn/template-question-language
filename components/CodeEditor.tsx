8acbc15497a647035a32821a28138c5159ebce70
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-07-29 19:03:37 -0700

    code editors are hard

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,28 +1,138 @@
-import { useEffect } from "react"
-
+import { FormEvent, FormEventHandler, useEffect, useRef, useState } from "react"
+import Editor from "react-simple-code-editor";
+import { runParser } from "../lib/language/runParser";
 // const Prism = require("./prism")
-import Prism from "./prism.js"
+import Prism, { highlight } from "./prism.js"
+Prism.languages.retmajgau = {
+    myText: { pattern: /[^}\n](?![^{]*})/ },
+    keyWords: { pattern: /if|then|else/ },
+    boolean: { pattern: /true|false/ },
+    // assignVariable:/[a-zA-Z]+=[^=]+/
+    isEqual: { pattern: /==/ },
+
+    myVariable: { pattern: /[a-zA-Z]+(?!=)=(?!=)/ }, ///[a-zA-Z]+(?!.*=)/,
+    WS: { pattern: /[ \t]+/ },
+    comment: { pattern: /\/\/.*?$/ },
+    // number: /0|[1-9][0-9]*/,
+    number: { pattern: /[0-9]+/ },
+    string: { pattern: /"(?:\\["\\]|[^\n"\\])*"/ },
+    plus: { pattern: /\+/ },
+    lparen: { pattern: /\(/ },
+    rparen: { pattern: /\)/ },
+    lbrace: { pattern: /\{/ },
+    rbrace: {
+        pattern: /}/
+    },
+    identifier: { pattern: /[a-zA-Z][a-zA-Z_0-9]*/ },
+    fatarrow: { pattern: /=>/ },
+    assign: { pattern: /=/ },
+}
+
+interface LiveCodeEditorProps {
+    defaultCode: string;
+}
+
+export const LiveCodeEditor: React.FC<LiveCodeEditorProps> = (props) => {
+    const { defaultCode } = props
+    const [code, setCode] = useState(defaultCode)
+    const outputRef = useRef(null)
+    function onChange(_event: any, newCode: string) {
+        setCode(newCode)
+        outputRef.current.innerText = runParser(newCode);
+    }
+    return <>
+        <CodeEditor
+            code={defaultCode}
+            editable
+            onChange={onChange}
+        />
+        <div ref={outputRef} className="output" />
+    </>
+}
+
 interface CodeEditorProps {
     editable?: boolean
     code: string;
+    onChange?: (e: FormEvent<HTMLDivElement>, value: string) => any;
 }
 const CodeEditor: React.FC<CodeEditorProps> = (props) => {
-    const { editable, code } = props
-    useEffect(() => {
-        Prism.highlightAll()
-        // console.log("useEffect");
-        // // test.current.innerText=props.children
-        // return () => {
-        //   console.log("unmount");
-        // };
-    }, [props])
+    const { editable, code, onChange } = props
+    const [rcode, setCode] = useState(code);
     return (
         <>
-            <pre >
-                <code id="help-me" className="language-python">
-                    {code}
-                </code>
-            </pre>
+            <Editor
+                onChange={(e) => onChange && onChange(e, rcode)}
+                value={rcode}
+                onValueChange={(rcode) => setCode(rcode)}
+                highlight={(code) => highlight(code, Prism.languages.retmajgau)}
+                padding={10}
+                style={{
+                    fontFamily: '"Fira code", "Fira Mono", monospace',
+                    background: "#272822",
+
+                    fontSize: 12,
+                    color: "#f8f8f2",
+                    // background: "none",
+                    textShadow: "0 1px rgba(0, 0, 0, 0.3)",
+                    // fontFamily: "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+                    // fontSize: "1em",s
+
+                }}
+            />
+
+            <style>{`
+            .myText { 
+                color: white;
+             }
+            .keyWords { 
+                color: blue
+            }
+            .boolean { 
+                color: Chartreuse;
+             }
+            .isEqual { 
+                color: orange
+             }      
+            .myVariable { 
+                color: yellow;
+             }
+            .WS {
+                color: grey;
+             }
+            .comment { 
+                color: blue;
+            }
+            .number { 
+                color: Chartreuse;
+             }
+            .string {  
+                color: yellow;
+            }
+            .plus { 
+                color: aqua;
+            }
+            .lparen { 
+                color: red;
+            }
+            .rparen { 
+                color: orange
+            }
+            .lbrace { 
+                color: blueViolet;
+             }
+            .rbrace {
+                color: blueViolet;
+            }
+            .identifier { 
+                color: pink
+             }
+            .fatarrow { 
+                color: red;
+             }
+            .assign: {
+                color: purple
+            }
+            `}</style>
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { ChangeEventHandler, TextareaHTMLAttributes, useRef, useState } from 'react'
-import CodeEditor from '../components/CodeEditor'
+import React, { ChangeEventHandler, TextareaHTMLAttributes, useRef, useState } from 'react'
+import { LiveCodeEditor } from '../components/CodeEditor'
 import { runParser } from '../lib/language/runParser'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const textareaRef = useRef(null)
-  const outputaRef = useRef(null)
-  const [result, setResult] = useState("")
+  // const textareaRef = useRef(null)
+  // const outputaRef = useRef(null)
+  // const [result, setResult] = useState("")
 
   // function handleCompile() {
   //   fetch("/api/hello", { method: "post", body: textareaRef.current.value }).then(e => e.text())
@@ -16,19 +16,23 @@ export default function Home() {
   //       setResult(data);
   //     })
   // }
-  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    // setResult(runParser(e.target.value))
-    outputaRef.current.textContent = runParser(e.target.value)
-  }
+  // const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+  //   // setResult(runParser(e.target.value))
+  //   outputaRef.current.textContent = runParser(e.target.value)
+  // }
   return (
     <>
-      <CodeEditor
-        code="I am some code"
+      <LiveCodeEditor
+        defaultCode={`{a=1+1} \n{increment a} \nsome text`}
+      />
+      {/* <CodeEditor
+        code={`{a=1+1} \n {increment a} \n some text`}
+        onChange
         editable
       />
       {/* <textarea style={{ width: "50vw", height: "50vh" }} onChange={handleChange} ref={textareaRef} placeholder="type something" /> */}
       {/* <button>Compile</button> */}
-      <pre ref={outputaRef} className="output"></pre>
+      {/* <pre ref={outputaRef} className="output"></pre> */}
     </>
   )
 }
